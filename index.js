742eb843b2555ad377eb5ec964738b18295b619a
RakibulIslam <rakibulislamsofficial@gmail.com>
2021-10-29 11:01:08 +0600

    Package API added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 
 const app = express();
@@ -11,7 +12,26 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvyuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+const run = async () => {
+    await client.connect();
+    console.log('DB connected');
+    const database = client.db('Travelzen');
+    const packageCollection = database.collection('packages');
+
+    // Get packages API
+    app.get('/packages', async (req, res) => {
+        const cursor = packageCollection.find({});
+        const packages = await cursor.toArray();
+        res.send(packages);
+    })
+
+};
+
+run().catch(console.dir);
 
 
 
